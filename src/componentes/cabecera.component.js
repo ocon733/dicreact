413d4f8d8376639212babe9c3ae9515385652511
ocon733 <ocon0a@gmail.com>
2020-02-04 11:50:21 +0100

    validaciones parametros proptypes

--- src/componentes/cabecera.component.js
+++ src/componentes/cabecera.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './cabecera.component.css';
-
+import PropTypes from 'prop-types';
 class Cabecera extends React.Component{
 
    /*
@@ -8,13 +8,18 @@ class Cabecera extends React.Component{
         super(props);
     }
 */
+
     render(){
         return(
             <div className="caja">
     <h4>{this.props.titulo}</h4>
             </div>
         );
-    }
+    }   
 }
+
+Cabecera.propTypes= {
+      titulo : PropTypes.string.isRequired            
+     }
 
 export default Cabecera;
